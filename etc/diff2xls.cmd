6c777f3e8931d5fad59b62402e9c98e47c8b390c
Java Upgrade Agent <h-kato@js2.so-net.ne.jp>
2026-05-09 18:03:14 +0900

    update jar file name

--- etc/diff2xls.cmd
+++ etc/diff2xls.cmd
@@ -1,4 +1,4 @@
-set DIFF2XLS=%USERPROFILE%\lib\diff2xls-1.0-shaded.jar
+set DIFF2XLS=%USERPROFILE%\lib\diff2xls-1.2-shaded.jar
 set TEMPLATE=%USERPROFILE%\lib\template.xlsx
 
 java -jar %DIFF2XLS% %TEMPLATE% %*
